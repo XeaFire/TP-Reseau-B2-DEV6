527deba4a1410ffc6f763a2f8f407d55c677b694
DTristan <tristan.diard@ynov.com>
2024-10-30 12:44:01 +0100

    TP6

---
+++ part2/chat_server_ii_3.py
@@ -0,0 +1,44 @@
+import asyncio
+
+host = '10.33.49.148'
+port = 14447
+
+
+async def handle_packet(reader, writer):
+    servermessage = f"Hello {addr[0]}:{addr[1]}".encode("utf-8")
+    writer.write(servermessage)
+    await writer.drain()
+    
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received {message!r} from {addr!r}")
+
+    print(f"Send: {message!r}")
+    servermessage = f"Hello {addr[0]}:{addr[1]}".encode("utf-8")
+    writer.write(servermessage)
+    await writer.drain()
+    
+
+    # Je laisse ça là ça peut toujours me servir
+
+    # print("Close the connection")
+    # writer.close()
+    # await writer.wait_closed()
+    return
+
+
+
+async def main():
+    server = await asyncio.start_server(handle_packet, host, port)
+
+    addr = server.sockets[0].getsockname()
+    print(f"Serveur en écoute sur {addr}")
+
+    async with server:
+        await server.serve_forever()    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
