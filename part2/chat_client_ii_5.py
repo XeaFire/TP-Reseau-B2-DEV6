585c3719533928f9a81e2d9e2b7ef2dc4d07eb0b
DTristan <tristan.diard@ynov.com>
2024-10-31 10:25:28 +0100

    TP6

--- part2/chat_client_ii_5.py
+++ part2/chat_client_ii_5.py
@@ -48,11 +48,12 @@ async def main():
     while True:
         print("Veuillez choisir un pseudo:")
         username = input()
-        if re.match(username, r'^[a-zA-Z0-9_]{3,16}$'):
-            break
-        else:
-            print("Pseudo invalide veuillez choisir un pseudo de 3 à 16 characteres") 
-            continue
+        break
+        # if re.match(username, r'^[a-zA-Z0-9_]{3,16}$'):
+        #     break
+        # else:
+        #     print("Pseudo invalide veuillez choisir un pseudo de 3 à 16 characteres") 
+        #     continue
     
     msg = username.encode("utf-8")
     writer.write(msg)
